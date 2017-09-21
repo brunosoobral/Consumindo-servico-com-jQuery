1e3979859a0a5a0e1681f223c2440754c625e0f6
Bruno Sobral <bruno.soobral@gmail.com>
2017-09-21 15:51:58 -0300

    Atualização

--- assets/js/scripts/personagem.js
+++ assets/js/scripts/personagem.js
@@ -14,8 +14,84 @@ Carregar = function() {
         },
         error: function(e) {
             //Verificando erro..
+            //console.log(e);
             if(e.status) {
-                alert(e.responseJSON.Message);
+                $("#resultado").html("<div class='alert alert-danger'>"+e.responseJSON+"</div>");
+                return false;
+            }
+        }
+    });
+};
+
+Criar = function(dados) {
+    //Verificar se o usuário está autenticado..
+    Autenticado();
+
+    $.ajax({
+        url: Global.urlBase + "/personagem/criar",
+        type: "POST",
+        headers: { 'Authorization': 'Bearer ' + Global.token, 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: JSON.parse('{"Nickname":"'+dados.Nome+'" ,"Classe":"'+dados.Classe+'"}'),
+        cache: false,
+        async: false,
+        success: function(json) {
+            //Armazenando dados dos personagens..
+            Global.dados = json;
+        },
+        error: function(e) {
+            //Verificando erro..
+            if(e.status) {
+                $("#resultado").html("<div class='alert alert-danger'>"+e.responseJSON+"</div>");
+                return false;
+            }
+        }
+    });
+};
+
+Recriar = function(dados) {
+    //Verificar se o usuário está autenticado..
+    Autenticado();
+
+    $.ajax({
+        url: Global.urlBase + "/personagem/recriar",
+        type: "POST",
+        headers: { 'Authorization': 'Bearer ' + Global.token, 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: JSON.parse('{"Nickname":"'+dados.Nome+'"}'),
+        cache: false,
+        async: false,
+        success: function(json) {
+            //Armazenando dados dos personagens..
+            Global.dados = json;
+        },
+        error: function(e) {
+            //Verificando erro..
+            if(e.status) {
+                $("#resultado").html("<div class='alert alert-danger'>"+e.responseJSON+"</div>");
+                return false;
+            }
+        }
+    });
+};
+
+Excluir = function(dados) {
+    //Verificar se o usuário está autenticado..
+    Autenticado();
+
+    $.ajax({
+        url: Global.urlBase + "/personagem/excluir",
+        type: "POST",
+        headers: { 'Authorization': 'Bearer ' + Global.token, 'Content-Type': 'application/x-www-form-urlencoded' },
+        data: JSON.parse('{"Nickname":"'+dados.Nome+'"}'),
+        cache: false,
+        async: false,
+        success: function(json) {
+            //Armazenando dados dos personagens..
+            Global.dados = json;
+        },
+        error: function(e) {
+            //Verificando erro..
+            if(e.status) {
+                $("#resultado").html("<div class='alert alert-danger'>"+e.responseJSON+"</div>");
                 return false;
             }
         }
